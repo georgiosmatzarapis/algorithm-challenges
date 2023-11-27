d6e7ad89ccd47906d29ce0bed5c270d4bb823ee5
Georgios Matzarapis <142222522+georgiosmatzarapis@users.noreply.github.com>
2023-11-27 17:21:35 +0100

    MinimumWaitingTime

---
+++ greedy-algorithms/MinimumWaitingTime.cpp
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <cstdint>
+#include <iostream>
+#include <vector>
+
+int minimumWaitingTime(std::vector<int>& ioQueries) {
+  std::uint8_t aTotalWaiting{};
+  std::sort(ioQueries.begin(), ioQueries.end());
+
+  for (std::vector<int>::iterator it = ioQueries.begin(); it != ioQueries.end();
+       ++it) {
+    const auto index{std::distance(ioQueries.begin(), it)};
+    if (index != 0) {
+      for (int i = 0; i < index; ++i) {
+        const int& aQuery{ioQueries[i]};
+        aTotalWaiting += aQuery;
+      }
+    }
+  }
+  return aTotalWaiting;
+}
+
+int main() {
+  std::vector<int> aQueries{1, 1, 1, 4, 5, 6, 8, 1, 1, 2, 1};
+  std::cout << minimumWaitingTime(aQueries) << std::endl;
+  return 0;
+}
